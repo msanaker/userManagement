43889711ff3763cba50ea760d81a952c5188ba70
msanaker <matthew@sanaker.com>
2015-02-05 22:43:55 -0500

    initial check-in

---
+++ userContext/userContext.cs
@@ -0,0 +1,126 @@
+﻿/// <summary>
+///    Copyright 2015 Matthew Sanaker, matthew@sanaker.com, @msanaker on GitHub
+///    
+///    This file is part of userContext.
+///
+///    userContext is free software: you can redistribute it and/or modify
+///    it under the terms of the GNU General Public License as published by
+///    the Free Software Foundation, either version 3 of the License, or
+///    (at your option) any later version.
+///
+///    userContext is distributed in the hope that it will be useful,
+///    but WITHOUT ANY WARRANTY; without even the implied warranty of
+///    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///    GNU General Public License for more details.
+///
+///    You should have received a copy of the GNU General Public License
+///    along with userContext.  If not, see <http://www.gnu.org/licenses/>.
+/// </summary>
+
+using System;
+using System.Security.Authentication;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+
+namespace userManagement
+{
+    /// <summary>
+    /// userContext allows for AD authentication and gives the abiltiy to present an Active Directory user to an applicaiton including group memebership
+    /// </summary>
+    class userContext
+    {
+        public bool loggedOn { get; private set; }
+        public UserPrincipal SAMAccount { get; private set; }
+        public PrincipalSearchResult<Principal> currentUserAuthorizationGroups { get; private set; }
+
+        /// <summary>
+        /// Authenticates with Active Directory and returns a populated userContext object if authentication is successful
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static userContext authenticateUser(string userName, string password)
+        {
+            try
+            {
+                userContext uContext = new userContext();
+                bool authenticated = false;
+                PrincipalContext domainContext = new PrincipalContext(ContextType.Domain, Properties.Settings.Default.domain, Properties.Settings.Default.domainLDAPbase);
+                authenticated = domainContext.ValidateCredentials(userName, password, ContextOptions.Negotiate);
+                if (authenticated)
+                {
+                    uContext.loggedOn = true;
+                    uContext.SAMAccount = userContext.getUserAccount(userName);
+                    uContext.currentUserAuthorizationGroups = userContext.getGroups(userName);
+                }
+                else
+                {
+                    uContext.loggedOn = false;
+                }
+                return uContext;
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException ex)
+            {
+                throw ex;
+            } 
+        }
+
+        /// <summary>
+        /// Get an Active Directory User account by user name
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
+        public static UserPrincipal getUserAccount(string userName)
+        {
+            try
+            {
+                PrincipalContext domainContext = new PrincipalContext(ContextType.Domain, Properties.Settings.Default.domain, Properties.Settings.Default.domainLDAPbase);
+                UserPrincipal thisUser = UserPrincipal.FindByIdentity(domainContext, userName);
+                return thisUser;
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException ex)
+            {
+                throw ex;
+            } 
+        }
+
+        /// <summary>
+        /// Get an Active Directory User account by GUID
+        /// </summary>
+        /// <param name="userGuid"></param>
+        /// <returns></returns>
+        public static UserPrincipal getUserAccount(Guid userGuid)
+        {
+            try
+            {
+                PrincipalContext domainContext = new PrincipalContext(ContextType.Domain, Properties.Settings.Default.domain, Properties.Settings.Default.domainLDAPbase);
+                UserPrincipal thisUser = UserPrincipal.FindByIdentity(domainContext, IdentityType.Guid, userGuid.ToString());
+                return thisUser;
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException ex)
+            {
+                throw ex;
+            } 
+        }
+
+        /// <summary>
+        /// Get Active Directory group membership for a user account
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
+        public static PrincipalSearchResult<Principal> getGroups(string userName)
+        {
+            try
+            {
+            PrincipalContext domainContext = new PrincipalContext(ContextType.Domain, Properties.Settings.Default.domain, Properties.Settings.Default.domainLDAPbase);
+            UserPrincipal thisUser = UserPrincipal.FindByIdentity(domainContext, userName);
+            PrincipalSearchResult<Principal> groups = thisUser.GetGroups();
+            return groups;
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException ex)
+            {
+                throw ex;
+            } 
+        }
+    }
+}
---
+++ userContext/userManagement.cs
@@ -0,0 +1,196 @@
+﻿/// <summary>
+///    Copyright 2015 Matthew Sanaker, matthew@sanaker.com, @msanaker on GitHub
+///    
+///    This file is part of userManagement.
+///
+///    userManagement is free software: you can redistribute it and/or modify
+///    it under the terms of the GNU General Public License as published by
+///    the Free Software Foundation, either version 3 of the License, or
+///    (at your option) any later version.
+///
+///    userManagement is distributed in the hope that it will be useful,
+///    but WITHOUT ANY WARRANTY; without even the implied warranty of
+///    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///    GNU General Public License for more details.
+///
+///    You should have received a copy of the GNU General Public License
+///    along with userManagement.  If not, see <http://www.gnu.org/licenses/>.
+/// </summary>
+
+using System;
+using System.Security.Authentication;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.Globalization;
+
+namespace userManagement
+{
+    /// <summary>
+    /// accountManagement allows for Active Directory account creation and editing following particular user account naming conventions
+    /// User accounts are created using a firstName.lastName convention
+    /// </summary>
+    class ADaccountManagement
+    {
+        /// <summary>
+        /// Creates an Active Directory user using a firstName.lastName convention in the default Users container for the domain 
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <returns>UserPrincipal</returns>
+        public static UserPrincipal createUser(string firstName, string lastName, string password)
+        {
+            try
+            {
+                PrincipalContext domainContext = new PrincipalContext(ContextType.Domain, Properties.Settings.Default.domain, Properties.Settings.Default.domainLDAPbase);
+                UserPrincipal newUser = new UserPrincipal(domainContext);
+                newUser.GivenName = new CultureInfo("en-US").TextInfo.ToTitleCase(firstName);
+                newUser.Surname = new CultureInfo("en-US").TextInfo.ToTitleCase(lastName);
+                string display = new CultureInfo("en-US").TextInfo.ToTitleCase(firstName + " " + lastName);
+                newUser.Name = display;
+                newUser.DisplayName = display;
+                newUser.SamAccountName = firstName.ToLowerInvariant() + "." + lastName.ToLowerInvariant();
+                newUser.Save();
+                newUser.SetPassword(password);
+                newUser.ExpirePasswordNow();
+                newUser.Enabled = true;
+                return newUser;
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException ex)
+            {
+                throw ex;
+            } 
+        }
+
+        /// <summary>
+        /// Creates an Active Directory user using a firstName.lastName convention in the specificied OU in the domain
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="OU"></param>
+        /// <returns>UserPrincipal</returns>
+        public static UserPrincipal createUser(string firstName, string lastName, string password, string OU)
+        {
+            try
+            {
+                string LDAPpath = "OU=" + OU + "," + Properties.Settings.Default.domainLDAPbase;
+                PrincipalContext domainContext = new PrincipalContext(ContextType.Domain, Properties.Settings.Default.domain, LDAPpath);
+                UserPrincipal newUser = new UserPrincipal(domainContext);
+                newUser.GivenName = new CultureInfo("en-US").TextInfo.ToTitleCase(firstName);
+                newUser.Surname = new CultureInfo("en-US").TextInfo.ToTitleCase(lastName);
+                string display = new CultureInfo("en-US").TextInfo.ToTitleCase(firstName + " " + lastName);
+                newUser.Name = display;
+                newUser.DisplayName = display;
+                newUser.SamAccountName = firstName.ToLowerInvariant() + "." + lastName.ToLowerInvariant();
+                newUser.Save();
+                newUser.SetPassword(password);
+                newUser.ExpirePasswordNow();
+                newUser.Enabled = true;
+                return newUser;
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Creates an Active Directory user using a firstName.lastName convention in the specificied OU in the domain by making a copy of an existing user
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="password"></param>
+        /// <param name="OU"></param>
+        /// <param name="userToCopy"></param>
+        /// <returns></returns>
+        public static UserPrincipal createUser(string firstName, string lastName, string password, string OU, UserPrincipal userToCopy)
+        {
+            try
+            {
+                string LDAPpath = "OU=" + OU + "," + Properties.Settings.Default.domainLDAPbase;
+                PrincipalContext domainContext = new PrincipalContext(ContextType.Domain, Properties.Settings.Default.domain, LDAPpath);
+                UserPrincipal newUser = new UserPrincipal(domainContext);
+                newUser.GivenName = new CultureInfo("en-US").TextInfo.ToTitleCase(firstName);
+                newUser.Surname = new CultureInfo("en-US").TextInfo.ToTitleCase(lastName);
+                string display = new CultureInfo("en-US").TextInfo.ToTitleCase(firstName + " " + lastName);
+                newUser.Name = display;
+                newUser.DisplayName = display;
+                newUser.SamAccountName = firstName.ToLowerInvariant() + "." + lastName.ToLowerInvariant();
+                string[] upnParts = userToCopy.UserPrincipalName.Split(new Char [] {'@'});
+                newUser.UserPrincipalName = newUser.SamAccountName + "@" + upnParts[1];
+                newUser.Save();
+                newUser.SetPassword(password);
+                newUser.ExpirePasswordNow();
+                PrincipalSearchResult<Principal> groups = userContext.getGroups(userToCopy.SamAccountName);
+                foreach (Principal g in groups)
+                {
+                    GroupPrincipal group = GroupPrincipal.FindByIdentity(domainContext, g.Name);
+                    group.Members.Add(domainContext, IdentityType.UserPrincipalName, newUser.SamAccountName);
+                    group.Save();
+                }
+                string company = getProperty(userToCopy, "Company");
+                if (!(string.IsNullOrEmpty(company)))
+                {
+                    setProperty(newUser, "Company", company);
+                }
+                string dept = getProperty(userToCopy, "Department");
+
+                newUser.Enabled = true;
+                return newUser;
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException ex)
+            {
+                throw ex; 
+            } 
+        }
+
+        /// <summary>
+        /// Gets the value of a directory entry property for a given AD Principal object
+        /// </summary>
+        /// <param name="principal"></param>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        private static String getProperty(Principal principal, string property)
+        {
+            try
+            {
+            string value;
+            DirectoryEntry dirEntry = principal.GetUnderlyingObject() as DirectoryEntry;
+            if (dirEntry.Properties.Contains(property))
+                value = dirEntry.Properties[property].Value.ToString();
+            else
+                value = String.Empty;
+            return value;
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        /// <summary>
+        /// Sets the value of a directory entry property for a given AD Principal object
+        /// </summary>
+        /// <param name="principal"></param>
+        /// <param name="property"></param>
+        /// <param name="value"></param>
+        private static void setProperty(Principal principal, string property, string value)
+        {
+            try
+            {
+                DirectoryEntry dirEntry = principal.GetUnderlyingObject() as DirectoryEntry;
+                if (dirEntry.Properties.Contains(property))
+                {
+                    dirEntry.Properties[property].Value = value;
+                }
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException ex)
+            {
+                throw ex;
+            }
+        }
+    
+    }
+
+
+}
